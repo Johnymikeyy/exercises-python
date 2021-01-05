657a1f740a94f7c0607156e7ff93ad23be86fef6
John <johnymikeyy@gmail.com>
2021-01-05 21:58:07 +0100

    euler1

---
+++ project_ecu/multiples3and5.py
@@ -0,0 +1,8 @@
+naturel_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+a = naturel_numbers[1] * naturel_numbers[3]
+b = naturel_numbers[1] * naturel_numbers[5]
+c = naturel_numbers[2] * naturel_numbers[3]
+d = naturel_numbers[1] * naturel_numbers[-1]
+
+print(a+b+c+d)
