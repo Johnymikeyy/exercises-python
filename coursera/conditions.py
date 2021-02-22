f85173f197565bfdbb2f8de0104b6df36b4963c3
fgok <fgokdata@gmail.com>
2021-02-22 21:55:40 +0100

    conditions

---
+++ coursera/conditions.py
@@ -0,0 +1,19 @@
+# Write your code below and press Shift+Enter to execute
+came = 1990
+if came < 1980 or came == 1991 or came ==1993:
+    print("this album came out in the year %d" %came)
+else:
+    print("this album came out in the year %d" %came)
+    
+
+album_year = 1979
+
+if album_year < 1980 or album_year == 1991 or album_year == 1993:
+    print ("This album came out in year %d" %album_year)
+    
+    
+came = 1991
+if came < 1980 or came == 1991 or came ==1993:
+    print("this album came out in the year %d" %came)
+    
+    
